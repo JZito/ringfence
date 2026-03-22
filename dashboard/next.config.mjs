42792b6caa9ef84c9b6728efdc2a1e5c310f92ba
J <oakisland@gmail.com>
2026-03-22 22:22:05 +0000

    bugfix: update next config

--- dashboard/next.config.mjs
+++ dashboard/next.config.mjs
@@ -1,7 +1,20 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 /** @type {import('next').NextConfig} */
 const API_URL = process.env.API_URL ?? "http://localhost:3000";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const nextConfig = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias ?? {}),
+      "@": __dirname,
+    };
+
+    return config;
+  },
   async rewrites() {
     return [
       {
